5019467da2d359c65ff94406cf6a52af545d83ae
Gabriellirapsr4 <130811624+Gabriellirapsr4@users.noreply.github.com>
2023-08-01 17:29:18 +0000

    adicionei divide

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ def som(valor1,valor2):
 
 def subtrai(valor1,valor2):
     return valor1-valor2
+
+def multiplica(valor1,valor2):
+    return valor1*valor2
+
+def divide(valor1,valor2):
+    return valor1/valor2
